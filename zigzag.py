d6fa1a40ff120161bcf01df012e45052963e4a34
Enmanuel de la Nuez <edelanuezlang@gmail.com>
2020-02-24 15:52:44 +0000

    add solutions

---
+++ zigzag.py
@@ -0,0 +1,16 @@
+def convert(s: str, num_rows: int) -> str:
+	rows = []
+	for _ in range(num_rows):
+		rows.append([])
+	count = 0
+	for char in s:
+		if count == 0:
+			isDown = True
+		elif count == num_rows - 1:
+			isDown = False
+		rows[count].append(char)
+		if isDown:
+			count += 1
+		else:
+			count -= 1
+	return "".join("".join(row) for row in rows)
